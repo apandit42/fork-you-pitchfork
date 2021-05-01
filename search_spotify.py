cbb6a748ca342f170f1a39742efe896850d6d86a
apandit42 <apandit@stanford.edu>
2021-04-30 21:41:04 -0700

    bungus check

--- search_spotify.py
+++ search_spotify.py
@@ -6,42 +6,60 @@ import keys
 
 
 # Build the query string for spotify search calls
-def get_query(artist, album, year=None):
-    if year is None:
-        q = f'album:{album} artist:{artist}'
-    else:
-        q = f'album:{album} artist:{artist} year:{year}'
-    q = q.replace('“','').replace('”','')
+def get_query(artist, album):
+    q = f'album:{album} artist:{artist}'
     return q
 
 
+# Clean all text and prep for searches
+def clean_text(text):
+    if pd.isna(text):
+        text = ''
+    elif isinstance(text, float):
+        text = int(text)
+    text = str(text)
+    text = text.strip().lower().replace('“','').replace('”','')
+    text = text.replace('’','').replace('','')
+    return text
+
+
+# Verify a list of albums
+def verify_album_list(album_list, wanted_album, wanted_artist):
+    verified_list = []
+    for album in album_list:
+        if verify_album_match(album, wanted_album, wanted_artist):
+            verified_list += [album]
+    return verified_list
+
+
 # Verify that the albums match the pitchfork album name, artist, and year
-def verify_album_match(album, wanted_album, wanted_artist, wanted_year=None):
-    if wanted_album != album.name:
+def verify_album_match(album, wanted_album, wanted_artist):
+    candidate_name = clean_text(album.name)
+    if wanted_album not in candidate_name and candidate_name not in wanted_album :
+        print(f'Names didnt match {wanted_album} vs {album.name}')
         return False
 
-    if wanted_year != None:
-        release_year = album.release_date.split('-')[0]
-        if release_year != wanted_year:
-            return False
+    # Check if artists match, first single artist case, then multi artist
     artists_found = album.artists
     if len(artists_found) == 1:
-        if artists_found[0].name != wanted_artist:
+        artist_name = clean_text(artists_found[0].name)
+        if artist_name not in wanted_artist and wanted_artist not in artist_name:
+            print(f'Artists dont match {artists_found[0].name} vs {wanted_artist}')
             return False
-    # if spotify has multiple artists listed for an album
+        else:
+            return True
     else:
-        #wanted artist: "Kanye West, Jay-Z"
-        #artists_found = ["Kanye West", "Jay-Z", "Tyler, the Creator"]
-        # potentially need to change this 
-        for artist in artists_found:
-            artist_name = artist.name
-            if artist_name not in wanted_artist:
-                return False
-    return True
+        wanted_artist_set = {clean_text(x) for x in wanted_artist.split(', ')}
+        artists_found_set = {clean_text(y) for x in artists_found for y in x.name.split(', ')}
+        if len(wanted_artist_set.intersection(artists_found_set)) == 0:
+            print(f'MULTI artists dont match {artists_found[0].name} vs {wanted_artist}')
+            return False
+        else:
+            return True
 
 
 # Build each album dict for the list
-def build_album_dict(album, pitchfork_id, artist, album_name, year):
+def build_album_dict(album, pitchfork_id, artist, album_name):
     artist_id = ','.join([x.id for x in album.artists])
     album_info_dict = {
         'pitchfork_id': pitchfork_id,
@@ -49,7 +67,6 @@ def build_album_dict(album, pitchfork_id, artist, album_name, year):
         'album_id': album.id,
         'artist': artist,
         'artist_id': artist_id,
-        'year': year,
     }
     return album_info_dict
 
@@ -66,67 +83,66 @@ def spotify_scraper(df, client_id=keys.AYUSH_SPOTIFY_CLIENT_ID, client_secret=ke
     # Counter of found results + not found results
     found_results = 0
     not_found_results = 0
+    counter = 0
 
     # loop through each row in pitchfork data to find matching album on spotify 
-    for pitchfork_id, artist, album_name, year in df.itertuples(index=False, name=None):
-        # Check if year NaN, and if so, set to none
-        if pd.isna(year):
-            year = None
-        
-        # Confirm run printing
-        print(f'ID {pitchfork_id} w/ {album_name} by {artist} ({int(year)})...')
-        
-        # Albums to verify before building
+    for pitchfork_id, artist, album_name in df.itertuples(index=False, name=None):
+        # Clean variables, set flag, & print confirmation
+        artist = clean_text(artist)
+        album_name = clean_text(album_name)
         candidate_matches = []
+        print(f'ID {pitchfork_id} w/ {album_name} by {artist} ...')
 
-        # Boolean flag if this album has been found
-        album_found = False
+        # if counter < 7800:
+        #     counter += 1
+        #     continue
 
         # Build search query & get initial search results
-        q = get_query(artist, album_name, year)
+        q = get_query(artist, album_name)
         albums, = spotify.search(q, types=('album',))
+        candidate_matches += verify_album_list(albums.items, album_name, artist)
         
-        # If no results found and multiple comma separated artists in artist text (with Tyler edge case)
-        if albums.total == 0 and artist.find(', ') != -1:
+        # First try if its an ep, removing ep
+        if len(candidate_matches) == 0 and album_name.endswith(' ep', -3):
+            q = get_query(artist, album_name[:-3])
+            albums, = spotify.search(q, types=('album',))
+            candidate_matches += verify_album_list(albums.items, album_name[:-3], artist)
+        
+        # If that's not enough, try splitting up the artist
+        if len(candidate_matches) == 0 and artist.find(', ') != -1:
             # Split into multiple artists with delimiter
-            query_artist_list = artist.split(', ')
+            query_artist_list = [clean_text(x) for x in artist.split(', ')]
 
             # Loop results and build new queries
             for curr_artist in query_artist_list:
-                q = get_query(curr_artist, album_name, year)
+                q = get_query(curr_artist, album_name)
                 albums, = spotify.search(q, types=('album',))
-                
-                # if new query turns up result, add to candidate albums
-                if albums.total != 0:
-                    candidate_matches += albums.items
-
-        # If match found on initial search with pitchfork artist name, add to candidates
-        else:
-            candidate_matches += albums.items
+                candidate_matches += verify_album_list(albums.items, album_name, artist)
         
         # Now loop through all albums and verify matches, appending verified results
         for candidate_album in candidate_matches:
-            if verify_album_match(candidate_album, album_name, artist, year):
-                new_match = build_album_dict(candidate_album, pitchfork_id, artist, album_name, year)
-                results.append(new_match)
-                album_found =  True
+            print(f'{candidate_album.name} by {candidate_album.artists[0].name}')
+            new_match = build_album_dict(candidate_album, pitchfork_id, artist, album_name)
+            results.append(new_match)
         
         # Modify found status
-        if album_found:
+        if len(candidate_matches):
             found_results += 1
         else:
+            print(f'\n*********************\nALBUM NOT FOUND: {album_name} by {artist}!\n**************************\n')
             not_found_results += 1
+        
+        counter += 1
 
     # Now print total found / not found
     print(f'TOTAL FOUND: {found_results} ({found_results/(found_results + not_found_results)}%)')
     print(f'TOTAL NOT FOUND {not_found_results} ({not_found_results/(found_results + not_found_results)}%)')
-
     return results
 
 
 if __name__ == '__main__':
     df = pd.read_csv('pitchfork_core.csv')
-    df = df[['pitchfork_id', 'artist', 'album', 'release_year']]
+    df = df[['pitchfork_id', 'artist', 'album']]
     result_list = spotify_scraper(df)
     output_df = pd.DataFrame(result_list)
     output_df.to_csv('spotify_album_and_artist_ids.csv', index=False)
